cc731bf8c727bb64fd2fe8ed5967df596cf54af6
unknown <oscar>
2022-07-22 17:16:32 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/sofka/spacialStationSofka/SpacialStationSofkaApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.spacialStationSofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacialStationSofkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacialStationSofkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/spacialStationSofka/SpacialStationSofkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.spacialStationSofka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacialStationSofkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
